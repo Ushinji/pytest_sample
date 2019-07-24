b1d3bea061cded5ad28ee7765be350a0f3d56f82
Ushinji <siguma971@gmail.com>
2019-07-24 09:49:50 +0900

    Add pytest using parametrize

--- tests/test_sum.py
+++ tests/test_sum.py
@@ -1,3 +1,4 @@
+import pytest
 from app.sum import sum
 
 
@@ -5,3 +6,11 @@ class TestSum():
 
     def test_answer(self):
         assert sum(1, 2) == 3
+
+    @pytest.mark.parametrize("a, b, expected", [
+        (1, 2, 3),
+        (2, 3, 5),
+        (4, 5, 9)
+    ])
+    def test_answer_using_parametrize(self, a, b, expected):
+        assert sum(a, b) == expected
