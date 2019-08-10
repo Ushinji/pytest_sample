79ceb8773e4d483bea4ae1c9f262cff218bcb8a0
Ushinji <siguma971@gmail.com>
2019-08-10 23:28:38 +0900

    Edit test_sum_view using parameterized_class

--- tests/test_views/api/test_sum_view.py
+++ tests/test_views/api/test_sum_view.py
@@ -1,7 +1,13 @@
 import json
+from parameterized import parameterized_class
 from tests import Base
 
 
+@parameterized_class(('a', 'b', 'expected_sum'), [
+    (1, 2, 3),
+    (2, 3, 5),
+    (4, 5, 9),
+])
 class TestApiSumViewInNormalCase(Base):
     '''GET /api/sum 正常系のテスト'''
 
@@ -13,17 +19,20 @@ class TestApiSumViewInNormalCase(Base):
 
     def test_return_200(self):
         '''200を返すこと'''
-        response = self.api_get('/api/sum', {'a': 1, 'b': 2})
+        response = self.api_get('/api/sum', {'a': self.a, 'b': self.b})
         assert response.status_code == 200
 
     def test_return_sum(self):
         '''クエリストリングで渡した値の和を返すこと'''
-        response = self.api_get('/api/sum', {'a': 1, 'b': 2})
+        response = self.api_get('/api/sum', {'a': self.a, 'b': self.b})
         data = json.loads(response.get_data(as_text=True))
 
-        assert data['result'] == 3
+        assert data['result'] == self.expected_sum
 
 
+@parameterized_class(('a', 'b'), [
+    ('A', 'B'),
+])
 class TestApiSumViewInAbNormalCase(Base):
     '''GET /api/sum 異常系のテスト'''
 
@@ -36,13 +45,14 @@ class TestApiSumViewInAbNormalCase(Base):
     def test_return_422(self):
         '''422を返すこと'''
         response = self.api_get(
-            '/api/sum', {'a': 'INVALID_A', 'b': 'INVALID_B'})
+            '/api/sum', {'a': self.a, 'b': self.b})
         assert response.status_code == 422
 
     def test_return_error_message(self):
         '''エラーメッセージを返すこと'''
         response = self.api_get(
-            '/api/sum', {'a': 'A', 'b': 'B'})
+            '/api/sum', {'a': self.a, 'b': self.b})
         data = json.loads(response.get_data(as_text=True))
 
-        assert data['errors'] == f'Query strings must be integer. a: A, b: B'
+        err_msg = f'Query strings must be integer. a: {self.a}, b: {self.b}'
+        assert data['errors'] == err_msg
